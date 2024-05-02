34d8923902449cc0b3934cbc4388f4dc30dbc770
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-05-02 13:04:04 +0700

    Use AddressCopyPopover, handle repairing in home screen

--- packages/stackup/src/app/homescreen/page.tsx
+++ packages/stackup/src/app/homescreen/page.tsx
@@ -3,7 +3,11 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { Client, Presets } from "userop";
 import { Button } from "@/components/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/popover";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 import { TextInput } from "@/components/textInput";
 import * as store from "@/mpc/storage/account";
 import { useRouter } from "next/navigation";
@@ -15,15 +19,16 @@ import { MoreVertical } from "lucide-react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { PasswordBackupScreen } from "@/components/password/passwordBackupScreen";
 import { signOut } from "@/mpc";
+import { AddressCopyPopover } from "@/components/addressCopyPopover";
 
 interface HomescreenProps {}
 
 const Homescreen: React.FC<HomescreenProps> = ({}) => {
+    const oldEoa = store.getOldEoa();
     const router = useRouter();
     const [walletAccount, setWalletAccount] = useState<store.accountType>();
     const [walletBalance, setWalletBalance] = useState<string>("0");
     const [eoa, setEoa] = useState<store.accountType>();
-    const [oldEoa, setOldEoa] = useState<store.accountType>();
     const [network, setNetwork] = useState("...");
     const [switchChain, setSwitchChain] = useState<"none" | "popup" | "button">(
         "none"
@@ -52,7 +57,6 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
         // get wallet account and eoa
         setWalletAccount(account);
         setEoa(eoa);
-        setOldEoa(store.getOldEoa());
     }, []);
 
     const SEPOLIA = {
@@ -260,7 +264,7 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
     const handleRecover = () => {
         if (eoa) {
             store.setOldEoa(eoa);
-            router.push("/pair");
+            router.push("/pair?repair=true");
         } // TODO: handle undefined eoa case
     };
 
@@ -303,8 +307,8 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                 <div className="text-center">
                                     {`${
                                         eoa === oldEoa
-                                            ? "Your Distributed AA wallet is ready! Add funds to your wallet from a faucet to get started!"
-                                            : "Your Silent Account is operational again!"
+                                            ? "Your Silent Account is operational again!"
+                                            : "Your Distributed AA wallet is ready! Add funds to your wallet from a faucet to get started!"
                                     }`}
                                 </div>
                             </div>
@@ -350,54 +354,14 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                     <div className="text-black-200 text-sm">
                                         My Wallet
                                     </div>
-                                    <Popover>
-                                        <PopoverTrigger>
-                                            <div
-                                                className="flex items-center"
-                                                onClick={async () => {
-                                                    navigator.clipboard.writeText(
-                                                        walletAccount?.address ||
-                                                            ""
-                                                    );
-                                                }}
-                                            >
-                                                <span className="mr-1 text-[black] b1-bold ">
-                                                    {walletAccount?.address.slice(
-                                                        0,
-                                                        5
-                                                    )}
-                                                    {"..."}
-                                                    {walletAccount?.address.slice(
-                                                        walletAccount.address
-                                                            .length - 5,
-                                                        walletAccount.address
-                                                            .length
-                                                    )}
-                                                </span>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="20"
-                                                    height="21"
-                                                    viewBox="0 0 20 21"
-                                                    fill="none"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        clipRule="evenodd"
-                                                        d="M4.37315 1.75L4.375 1.75H12.8125L12.8143 1.75C13.5591 1.75212 14.2728 2.04893 14.7994 2.57558C15.3261 3.10224 15.6229 3.81592 15.625 4.56072C15.625 4.56565 15.625 4.57059 15.6249 4.57552L15.6186 4.875H15.8984C17.4733 4.875 18.75 6.15169 18.75 7.72656V16.3984C18.75 17.9733 17.4733 19.25 15.8984 19.25H7.22656C5.65169 19.25 4.375 17.9733 4.375 16.3984V16.125H4.0625L4.06072 16.125C3.31592 16.1229 2.60224 15.8261 2.07558 15.2994C1.54893 14.7728 1.25212 14.0591 1.25 13.3143L1.25 13.3125V4.875L1.25 4.87315C1.25245 4.04559 1.58228 3.25263 2.16745 2.66745C2.75263 2.08228 3.54559 1.75245 4.37315 1.75ZM5.625 16.3984C5.625 17.283 6.34204 18 7.22656 18H15.8984C16.783 18 17.5 17.283 17.5 16.3984V7.72656C17.5 6.84204 16.783 6.125 15.8984 6.125H14.9826C14.9813 6.125 14.9801 6.125 14.9789 6.125H7.22656C6.34204 6.125 5.625 6.84204 5.625 7.72656V16.3984ZM14.3684 4.875H7.22656C5.65169 4.875 4.375 6.15169 4.375 7.72656V14.875H4.06351C3.64928 14.8736 3.25239 14.7085 2.95947 14.4155C2.66656 14.1226 2.50139 13.7258 2.5 13.3115V4.87599C2.50169 4.37898 2.69988 3.9028 3.05134 3.55134C3.4028 3.19988 3.87898 3.00169 4.37599 3H12.8115C13.2258 3.00139 13.6226 3.16656 13.9155 3.45947C14.207 3.75098 14.372 4.14545 14.375 4.55751L14.3684 4.875Z"
-                                                        fill="black"
-                                                    />
-                                                </svg>
-                                            </div>
-                                        </PopoverTrigger>
-                                        <PopoverContent
-                                            align="start"
-                                            side="right"
-                                            className="flex justify-center w-18 p-1 b2-md"
-                                        >
-                                            Copied!
-                                        </PopoverContent>
-                                    </Popover>
+                                    <AddressCopyPopover
+                                        className="text-[black] b1-bold"
+                                        address={
+                                            walletAccount?.address ||
+                                            "Address not found"
+                                        }
+                                    />
+
                                     {
                                         <div className="text-sm text-black-300 mt-2 font-bold">
                                             Balance: {walletBalance} ETH
@@ -411,48 +375,13 @@ const Homescreen: React.FC<HomescreenProps> = ({}) => {
                                     <div className="text-sm text-black-200">
                                         EOA
                                     </div>
-                                    <Popover>
-                                        <PopoverTrigger>
-                                            <div
-                                                className="flex items-center"
-                                                onClick={async () => {
-                                                    navigator.clipboard.writeText(
-                                                        eoa?.address || ""
-                                                    );
-                                                }}
-                                            >
-                                                <span className="mr-1 text-[black] b1-bold">
-                                                    {eoa?.address.slice(0, 5)}
-                                                    {"..."}
-                                                    {eoa?.address.slice(
-                                                        eoa.address.length - 5,
-                                                        eoa.address.length
-                                                    )}
-                                                </span>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="20"
-                                                    height="21"
-                                                    viewBox="0 0 20 21"
-                                                    fill="none"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        clipRule="evenodd"
-                                                        d="M4.37315 1.75L4.375 1.75H12.8125L12.8143 1.75C13.5591 1.75212 14.2728 2.04893 14.7994 2.57558C15.3261 3.10224 15.6229 3.81592 15.625 4.56072C15.625 4.56565 15.625 4.57059 15.6249 4.57552L15.6186 4.875H15.8984C17.4733 4.875 18.75 6.15169 18.75 7.72656V16.3984C18.75 17.9733 17.4733 19.25 15.8984 19.25H7.22656C5.65169 19.25 4.375 17.9733 4.375 16.3984V16.125H4.0625L4.06072 16.125C3.31592 16.1229 2.60224 15.8261 2.07558 15.2994C1.54893 14.7728 1.25212 14.0591 1.25 13.3143L1.25 13.3125V4.875L1.25 4.87315C1.25245 4.04559 1.58228 3.25263 2.16745 2.66745C2.75263 2.08228 3.54559 1.75245 4.37315 1.75ZM5.625 16.3984C5.625 17.283 6.34204 18 7.22656 18H15.8984C16.783 18 17.5 17.283 17.5 16.3984V7.72656C17.5 6.84204 16.783 6.125 15.8984 6.125H14.9826C14.9813 6.125 14.9801 6.125 14.9789 6.125H7.22656C6.34204 6.125 5.625 6.84204 5.625 7.72656V16.3984ZM14.3684 4.875H7.22656C5.65169 4.875 4.375 6.15169 4.375 7.72656V14.875H4.06351C3.64928 14.8736 3.25239 14.7085 2.95947 14.4155C2.66656 14.1226 2.50139 13.7258 2.5 13.3115V4.87599C2.50169 4.37898 2.69988 3.9028 3.05134 3.55134C3.4028 3.19988 3.87898 3.00169 4.37599 3H12.8115C13.2258 3.00139 13.6226 3.16656 13.9155 3.45947C14.207 3.75098 14.372 4.14545 14.375 4.55751L14.3684 4.875Z"
-                                                        fill="#ECEEF2"
-                                                    />
-                                                </svg>
-                                            </div>
-                                        </PopoverTrigger>
-                                        <PopoverContent
-                                            align="start"
-                                            side="right"
-                                            className="flex justify-center w-18 p-1 b2-md"
-                                        >
-                                            Copied!
-                                        </PopoverContent>
-                                    </Popover>
+                                    <AddressCopyPopover
+                                        className="text-[black] b1-bold"
+                                        address={
+                                            eoa?.address || "Address not found"
+                                        }
+                                    />
+
                                     <div className="mt-4 rounded-full bg-[#E8EDF3] text-sm py-2 px-3 flex flex-row">
                                         {network === "Sepolia Test Network" && (
                                             <img
