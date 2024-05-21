223983674f5941fc9b8a016fea0153efa2e9c9f5
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-05-21 15:28:17 +0700

    Use production Firebase api

--- packages/biconomy/src/mpc/transport/firebaseApi.ts
+++ packages/biconomy/src/mpc/transport/firebaseApi.ts
@@ -3,8 +3,8 @@
 
 import { MpcError, MpcErrorCode } from '../error';
 
-const baseUrl = 'https://us-central1-mobile-wallet-mm-snap-staging.cloudfunctions.net'
-// const baseUrl = 'https://us-central1-mobile-wallet-mm-snap.cloudfunctions.net'
+// const baseUrl = 'https://us-central1-mobile-wallet-mm-snap-staging.cloudfunctions.net'
+const baseUrl = 'https://us-central1-mobile-wallet-mm-snap.cloudfunctions.net'
 interface Response {
 	response: any;
 	error: string;
