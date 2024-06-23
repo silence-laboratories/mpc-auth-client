d8fe5a8683cfd49800c6115f8c645d6891cc0016
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-06-24 01:23:09 +0700

    Remove useSearchParams in switch screen hook

--- packages/biconomy/src/hooks/useSwitchScreen.tsx
+++ packages/biconomy/src/hooks/useSwitchScreen.tsx
@@ -1,20 +1,17 @@
 import { WALLET_STATUS } from "@/constants";
 import { getEoa, isPasswordReady } from "@/mpc/storage/account";
 import { getWalletStatus } from "@/mpc/storage/wallet";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+// This hook is used to switch screens based on the wallet state in FIRST time load
 export const useSwitchScreen = () => {
     const router = useRouter();
-    const query = useSearchParams();
-    const isRepairing = query.get("repair");
     const initializeScreen = () => {
         const status = getWalletStatus();
         const eoa = getEoa();
-        if ((status === WALLET_STATUS.Minted || eoa) && !isRepairing) {
+        if (status === WALLET_STATUS.Minted || eoa) {
             router.replace("/homescreen");
-        } else if ((status === WALLET_STATUS.Minted || eoa) && isRepairing) {
-            router.replace("/pair?repair=true");
         } else if (status === WALLET_STATUS.BackedUp) {
             router.replace("/mint");
         } else if (status === WALLET_STATUS.Paired && !isPasswordReady()) {
