cd8d758435877f77a68d003d24507b94e2bc18ea
Yogendra Sankhla <yogendra@silencelaboratories.com>
2024-06-25 16:51:00 +0530

    Update package.json scripts,Update index.ts for argument parsing

--- packages/biconomy/cli/srcMpc/types.d.ts
+++ packages/biconomy/cli/srcMpc/types.d.ts
@@ -1,5 +1,4 @@
 import { IP1KeyShare } from '@silencelaboratories/ecdsa-tss';
-import { KeyringAccount, KeyringRequest } from '@metamask/keyring-api';
 
 export interface Message {
 	message?: string;
