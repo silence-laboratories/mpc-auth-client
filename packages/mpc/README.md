68bbb32fd15f42fcdffe68de8b489601b9a44cc5
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-06-28 14:22:00 +0700

    docs(mpc): Add JSDoc for public class and function
    docs(mpc/README): Add README for mpc package

---
+++ packages/mpc/README.md
@@ -0,0 +1,51 @@
+# MPC Authenticator JS library
+
+## Components:
+
+The library provides the following 2 main components:
+
+1. MpcAuthenticator
+2. MpcSigner
+
+### MpcAuthenticator
+
+The `MpcAuthenticator` class is designed to handle authentication processes using Multi-Party Computation (MPC) techniques. This class provides a secure way to authenticate users without exposing sensitive information to any single party.
+
+An example of `MpcAuthenticator`:
+
+```javascript
+const mpcAuth = new MpcAuthenticator({
+  walletId: WALLET_ID,
+  storagePlatform: StoragePlatform.CLI,
+  customStorage: storage,
+  isDev: process.env.NODE_ENV === "development",
+});
+
+// Use Silent Shard app to scan this generated QR
+// Follow to install Silent Shard app: https://github.com/silence-laboratories/mpc-account-abstraction-sdk/tree/main/packages/biconomy/cli#step-4-using-the-silent-shard-app
+const qrCode = await mpcAuth.initPairing();
+
+// ... Scanning happens
+
+const pairingSessionData = await mpcAuth.runStartPairingSession();
+await mpcAuth.runEndPairingSession(pairingSessionData);
+
+const keygenResult = await mpcAuth.runKeygen(); // Retrieve our MPC keyshares after MPC Key Generation is done
+
+await mpcAuth.runBackup("demopassword"); // (Optional) Sent our backup for key restoration later
+
+```
+
+### MpcSigner
+
+The `MpcSigner` class is responsible for signing Ethereum transactions and messages. It is inherited from ethers.js `Signer` class, so we could use it with any ethers.js compatible interfaces out-of-the-box.
+
+An example of `MpcSigner`:
+```javascript
+const keygenResult = await mpcAuth.runKeygen(); // Required
+const p1KeyShare: IP1KeyShare = keygenResult.distributedKey.keyShareData;
+const publicKey = p1KeyShare.public_key;
+const address = ethers.utils.computeAddress(`0x04${publicKey}`);
+const mpcSigner = new MpcSigner(address, publicKey, p1KeyShare, keygenResult, mpcAuth); // Now, we could use mpcSigner to sign our transactions
+
+```
--- packages/mpc/src/domain/account.ts
+++ packages/mpc/src/domain/account.ts
@@ -4,6 +4,9 @@
 import { IStorage } from "../storage/types";
 import { AccountData, DeviceOS } from "../types";
 
+/**
+ * Manages account-related operations, interfacing with a storage mechanism to persist and retrieve account data.
+ */
 export class AccountManager {
   private storage: IStorage;
 
@@ -11,11 +14,19 @@ export class AccountManager {
     this.storage = storage;
   }
 
+  /**
+   * Creates an instance of AccountManager.
+   * @param {IStorage} storage - The storage mechanism for persisting account data.
+   */
   getEoa(): string | null {
     const storageData = this.storage.getStorageData();
     return storageData.eoa;
   }
 
+  /**
+   * Sets the smart contract account information in storage.
+   * @param {AccountData} walletAccount - The account data to be stored.
+   */
   setSmartContractAccount(walletAccount: AccountData) {
     if (!walletAccount.address) return;
     const storageData = this.storage.getStorageData();
@@ -23,21 +34,37 @@ export class AccountManager {
     this.storage.setStorageData(storageData);
   }
 
+  /**
+   * Retrieves the smart contract account information from storage.
+   * @returns {AccountData | null} The account data if available, otherwise null.
+   */
   getSmartContractAccount(): AccountData | null {
     const storageData = this.storage.getStorageData();
     return storageData.walletAccount ?? null;
   }
 
-  isPasswordReady() {
+  /**
+   * Checks if the password setup is complete.
+   * @returns {boolean} True if the password is set, false otherwise.
+   */
+  isPasswordReady(): boolean {
     return this.storage.getStorageData().passwordReady ?? false;
   }
 
-  setPasswordReady(isPasswordReady = true) {
+  /**
+   * Marks the password setup as complete or incomplete in storage.
+   * @param {boolean} [isPasswordReady=true] - Indicates whether the password setup is complete.
+   */
+  setPasswordReady(isPasswordReady: boolean = true) {
     const storageData = this.storage.getStorageData();
     storageData.passwordReady = isPasswordReady;
     this.storage.setStorageData(storageData);
   }
 
+  /**
+   * Determines the operating system of the device based on stored pairing data.
+   * @returns {DeviceOS} The operating system of the device.
+   */
   getDeviceOS(): DeviceOS {
     const storageData = this.storage.getStorageData();
     const deviceName = storageData.pairingData.deviceName;
