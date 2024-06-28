fa80abeb5339ca981be52c521476711f5a7103d2
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-06-28 11:15:09 +0700

    refactor(biconomy/cli): Remove firebase url form biconomy cli

--- packages/biconomy/cli/scripts/index.ts
+++ packages/biconomy/cli/scripts/index.ts
@@ -9,8 +9,7 @@ import { hideBin } from 'yargs/helpers';
 import { init } from "./init";
 import { getAddress } from "./address";
 import { nativeTransferPayERC20 } from "./erc20/nativeTransfer";
-import { erc20TransferPayERC20 } from "./erc20/erc20Transfer";
-import { mintNftPayERC20 } from "./erc20/mintNFT";
+
 
 const argv = yargs(hideBin(process.argv))
   .scriptName(chalk.green("smartAccount"))
@@ -62,89 +61,6 @@ const argv = yargs(hideBin(process.argv))
       console.log(
         chalk.magenta(`Transferring ${amount} ether to ${to}...`)
       );
-      if (argv.mode === "TOKEN") {
-        nativeTransferPayERC20(recipientAddress, amount);
-      } else {
-        nativeTransferPayERC20(recipientAddress, amount);
-      }
-    }
-  )
-  // Transfer an ERC20 token
-  .command(
-    "erc20Transfer",
-    chalk.blue("Transfer an ERC20 token"),
-    {
-      to: {
-        describe: chalk.cyan("Recipient address"),
-        demandOption: true,
-        type: "string",
-      },
-      amount: {
-        describe: chalk.cyan("Amount of tokens to transfer"),
-        demandOption: true,
-        type: "number",
-      },
-      token: {
-        describe: chalk.cyan("Token address"),
-        demandOption: true,
-        type: "string",
-      },
-      mode: {
-        describe: chalk.cyan("Paymaster mode"),
-        demandOption: false,
-        type: "string",
-      },
-    },
-    (argv) => {
-      const amount = argv.amount;
-      const tokenAddress = argv.token;
-      const recipientAddress = argv.to;
-      console.log(
-        chalk.magenta(
-          `Transferring ${amount} tokens of ${tokenAddress} to ${recipientAddress}...`
-        )
-      );
-      if (argv.mode === "TOKEN") {
-        erc20TransferPayERC20(recipientAddress, amount, tokenAddress);
-      }
-    }
-  )
-  // Mint nft token to SmartAccount
-  .command(
-    "mint",
-    chalk.blue("Mint nft token"),
-    {
-      mode: {
-        describe: chalk.cyan("Paymaster mode"),
-        demandOption: false,
-        type: "string",
-      },
-    },
-    (argv) => {
-      console.log(chalk.magenta("Minting an NFT token to the SmartAccount..."));
-      if (argv.mode === "TOKEN") {
-        mintNftPayERC20();
-      }
-    }
-  )
-  // Batch mint nft token to SmartAccount
-  .command(
-    "batchMint",
-    chalk.blue("Batch mint nft 2 times"),
-    {
-      mode: {
-        describe: chalk.cyan("Paymaster mode"),
-        demandOption: false,
-        type: "string",
-      },
-    },
-    (argv) => {
-      console.log(
-        chalk.magenta("Batch minting 2 NFT tokens to the SmartAccount...")
-      );
-    }
-  )
-  .help().argv;
       nativeTransferPayERC20(to, Number(amount));
     }
   )
