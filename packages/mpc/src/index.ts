2bfbd221813bb3190346f6ec28242ef946e15c10
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-07-01 10:02:39 +0700

    build(mpc): Export type StoragePlatform from entry point

--- packages/mpc/src/index.ts
+++ packages/mpc/src/index.ts
@@ -1,7 +1,8 @@
+import { StoragePlatform } from './types';
 // Copyright (c) Silence Laboratories Pte. Ltd.
 // This software is licensed under the Silence Laboratories License Agreement.
 
 import { MpcAuthenticator } from "./domain/authenticator";
 import { MpcSigner } from "./domain/signer";
 
-export { MpcAuthenticator, MpcSigner };
+export { MpcAuthenticator, MpcSigner, StoragePlatform };
