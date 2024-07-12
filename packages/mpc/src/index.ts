4658519c7ceb487be6955f4f3909ec99fbb93f37
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-07-12 11:53:32 +0700

    feat(mpc): Add singleton builder for MpcAuthenticator, export AccountData type

--- packages/mpc/src/index.ts
+++ packages/mpc/src/index.ts
@@ -1,8 +1,8 @@
-import { StoragePlatform, WalletId } from "./types";
 // Copyright (c) Silence Laboratories Pte. Ltd.
 // This software is licensed under the Silence Laboratories License Agreement.
 
+import { StoragePlatform, WalletId, AccountData } from "./types";
 import { MpcAuthenticator } from "./domain/authenticator";
 import { MpcSigner } from "./domain/signer";
 
-export { MpcAuthenticator, MpcSigner, StoragePlatform, WalletId };
+export { MpcAuthenticator, MpcSigner, StoragePlatform, WalletId, AccountData };
