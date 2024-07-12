519156b265d7c0cc34f76f20c959de24b8cbfb22
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2024-07-12 14:12:26 +0700

    fix(mpc): Make provider field public, remove getWalletId method

--- packages/mpc/src/storage/types.ts
+++ packages/mpc/src/storage/types.ts
@@ -8,6 +8,5 @@ export interface IStorage {
 	clearStorageData: () => void;
 	setStorageData: (data: StorageData) => void;
 	getStorageData: () => StorageData;
-	getWalletId: () => string;
 	migrate?: () => void;
 }
