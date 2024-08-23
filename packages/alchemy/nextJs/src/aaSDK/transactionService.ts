2e59baa7e954c42166e8fa0a925ba951f60d6742
Yogendra Sankhla <yogendra@silencelaboratories.com>
2024-08-23 11:58:22 +0530

    using lightAccount instead of modularAccount in alchemy,added paymaster sponsorship in cli pimlico

--- packages/alchemy/nextJs/src/aaSDK/transactionService.ts
+++ packages/alchemy/nextJs/src/aaSDK/transactionService.ts
@@ -1,10 +1,9 @@
 // Copyright (c) Silence Laboratories Pte. Ltd.
 // This software is licensed under the Silence Laboratories License Agreement.
-
 import { providers } from "ethers";
 import { MpcAuthenticator, MpcSigner } from "@silencelaboratories/mpc-sdk";
 import { ethersToAccount } from "./alchemyUtility";
-import { createModularAccountAlchemyClient } from "@alchemy/aa-alchemy";
+import { createLightAccountAlchemyClient } from "@alchemy/aa-alchemy";
 import { sepolia } from "@alchemy/aa-core";
 import * as viem from "viem";
 
@@ -13,24 +12,23 @@ export async function sendTransaction(
     amount: string,
     mpcAuth: MpcAuthenticator
 ) {
-    const eoa = mpcAuth.accountManager.getEoa();
-    if (!eoa) {
-        throw new Error("Eoa not found");
-    }
-    const provider = new providers.JsonRpcProvider("https://rpc.sepolia.org");
-    const client = await MpcSigner.instance(mpcAuth, provider);
-    const accountSigner = ethersToAccount(client);
-    const smartAccountClient = await createModularAccountAlchemyClient({
-        apiKey: process.env.API_KEY,
-        chain: sepolia,
-        signer: accountSigner,
-    });
-
-    const requestData = {
-        to: recipientAddress,
-        value: convertEtherToWei(amount),
-    };
     try {
+        const eoa = mpcAuth.accountManager.getEoa();
+        if (!eoa) {
+            throw new Error("Eoa not found");
+        }
+        const provider = new providers.JsonRpcProvider("https://rpc.sepolia.org");
+        const client = await MpcSigner.instance(mpcAuth, provider);
+        const accountSigner = ethersToAccount(client);
+        const smartAccountClient = await createLightAccountAlchemyClient({
+            apiKey: process.env.API_KEY,
+            chain: sepolia,
+            signer: accountSigner,
+        });
+        const requestData = {
+            to: recipientAddress,
+            value: convertEtherToWei(amount),
+        };
         const uo = await smartAccountClient.sendUserOperation({
             uo: {
                 target: requestData.to as viem.Hex,
@@ -38,15 +36,15 @@ export async function sendTransaction(
                 value: requestData.value,
             },
         });
-
         const transactionHash =
             await smartAccountClient.waitForUserOperationTransaction(uo);
         return { transactionHash };
     } catch (error) {
-        console.error("Transaction error:", error);
-        return { success: false, error };
+        console.error("Transaction failed", error);
+        throw new Error(`Transaction failed: ${error}`);
     }
 }
+
 function convertEtherToWei(etherString: string) {
     const ether = Number(etherString);
     const weiString = (ether * 1e18).toString();
